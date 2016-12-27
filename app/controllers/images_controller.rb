4e3f350ca20e2dea8926ef128bf8f76729819757
Gwen <wenqingou@gmail.com>
2016-12-27 18:41:07 +0000

    add in the login restriction and sign in upon sign up

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,9 @@
 class ImagesController < ApplicationController
+  
+  before_action :set_image, only: [:show, :destroy, :edit, :update]
+  before_action :require_user, only: [:new, :create, :edit, :update, :destroy]
+
+  
   def new 
     @image = Image.new
   end 
@@ -13,8 +18,20 @@ class ImagesController < ApplicationController
     end 
   end 
   
+  def edit
+    
+  end 
+  
+  def update
+    if @image.update(image_params)
+      flash[:success]="The image was updated successfully"
+      redirect_to image_path(@image)
+    else 
+      render 'edit'
+    end 
+  end 
+  
   def show 
-    @image =Image.find(params[:id]) 
   end 
   
   def index
@@ -22,7 +39,6 @@ class ImagesController < ApplicationController
   end
   
   def destroy
-    @image =Image.find(params[:id]) 
     if @image.destroy 
       flash[:notice]="image destroyed"
       redirect_to images_path
@@ -36,5 +52,10 @@ class ImagesController < ApplicationController
   def image_params 
     params.require(:image).permit(:picture)
   end 
+
+  def set_image
+    @image =Image.find(params[:id]) 
+  end 
+  
 
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_user, only: [:edit, :update ]
+  before_action :require_same_user, only: [:edit, :update]
   
   def new 
     @user = User.new
@@ -7,6 +10,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save 
+      session[:user_id]=@user.id
       flash[:success] = "Welcome to Crownshot"
       redirect_to user_path(@user)
     else
@@ -15,11 +19,9 @@ class UsersController < ApplicationController
   end 
   
   def edit 
-    @user = User.find(params[:id])
   end 
   
   def update 
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "You profile was updated successfully"
       redirect_to user_path(@user)
@@ -29,7 +31,6 @@ class UsersController < ApplicationController
   end 
   
   def show
-    @user = User.find(params[:id]) 
   end 
   
   def index
@@ -42,4 +43,14 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)  
   end 
   
+  def set_user
+     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to user_path(current_user)
+    end 
+  end 
 end 
