f435410beb1f510cb95e13f6a9e7a2e2b36207e8
Gwen <wenqingou@gmail.com>
2017-01-06 20:39:46 +0000

    added initial image preview function

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,14 +4,8 @@
 /* global regexp */
 
 
-// $(document).ready(function(){
-  
-//   $('form').on ('click', '.remove_fields', function(event){
-//     $(this).prev('input[type=hidden]').val('1');
-//     $(this).closest('fieldset').hide();
-//     event.preventDefault();
-//   });
-
+ $(document).ready(function(){
   
   
-// });
+  
+ });
