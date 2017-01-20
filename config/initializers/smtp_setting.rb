38af81bbdb75bca26b6128341b37139c1c2387a3
Gwen <wenqingou@gmail.com>
2017-01-20 16:25:32 +0000

    email try 11

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.delivery_method = :smtp  
 ActionMailer::Base.smtp_settings = {            
   :address              => "smtp.zoho.com", 
-  :port                 => '465',                 
+  :port                 => "587",                 
   :user_name            => 'crownshotty@gmail.com',
   :password             => 'templogin',     
   :authentication       => :login,
