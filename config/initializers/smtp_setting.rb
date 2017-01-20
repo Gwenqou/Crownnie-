4dae82f45fdc817c8f631c14277d7b8aa3605d12
Gwen <wenqingou@gmail.com>
2017-01-20 16:03:39 +0000

    style password reset pages

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -1,12 +1,12 @@
-ActionMailer::Base.delivery_method = :smtp  
-ActionMailer::Base.smtp_settings = {            
-  :address              => "smtp.zoho.com", 
-  :port                 => "587",                 
-  :user_name            => 'crownshotty@gmail.com',
-  :password             => 'templogin',         
-  :authentication       => :login,
-  #:ssl                  => true,
-  #:tls                  => true,
-  :enable_starttls_auto => true    
-}
+# ActionMailer::Base.delivery_method = :smtp  
+# ActionMailer::Base.smtp_settings = {            
+#   :address              => "smtp.zoho.com", 
+#   :port                 => "465",                 
+#   :user_name            => 'crownshotty@gmail.com',
+#   :password             => 'templogin',         
+#   :authentication       => :login,
+#   #:ssl                  => true,
+#   #:tls                  => true,
+#   :enable_starttls_auto => true    
+# }
 
