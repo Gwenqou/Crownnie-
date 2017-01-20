9f769daa51ac5a5a68023c53a4634142408da185
Gwen <wenqingou@gmail.com>
2017-01-20 16:09:43 +0000

    email send try 8

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -1,12 +1,13 @@
-# ActionMailer::Base.delivery_method = :smtp  
-# ActionMailer::Base.smtp_settings = {            
-#   :address              => "smtp.zoho.com", 
-#   :port                 => "465",                 
-#   :user_name            => 'crownshotty@gmail.com',
-#   :password             => 'templogin',         
-#   :authentication       => :login,
-#   #:ssl                  => true,
-#   #:tls                  => true,
-#   :enable_starttls_auto => true    
-# }
+ActionMailer::Base.delivery_method = :smtp  
+ActionMailer::Base.smtp_settings = {            
+  :address              => "smtp.zoho.com", 
+  :port                 => 465,                 
+  :user_name            => ENV['zoho_username'],
+  :password             => ENV['zoho_password'],         
+  :authentication       => :login,
+  #:ssl                  => true,
+  #:tls                  => true,
+  :enable_starttls_auto => true    
+}
+
 
