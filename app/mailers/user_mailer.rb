3a4e50c041f9cafddec7eea5322043d9db45f4d6
Gwen <wenqingou@gmail.com>
2017-01-20 15:32:05 +0000

    email send try 7

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, :subject => "Crownshot password_reset"
+    mail to: user.email, :subject => "Crownshot password reset"
   end
 end
