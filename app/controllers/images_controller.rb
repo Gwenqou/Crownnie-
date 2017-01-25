137d45c6743aaa7c805e2a68f373e85cc679e0be
Gwen <wenqingou@gmail.com>
2017-01-25 14:57:36 +0000

    take away order random and use shuffle instead

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -32,10 +32,10 @@ class ImagesController < ApplicationController
       if params[:category].present?
         category = Category.where('lower(name) = ?', params[:category].downcase).first
         category_id = category.id unless category.nil?
-        @images = Image.where(id: @images).joins(:categories).where(categories:{ id: category_id }).order("RANDOM()").paginate(page: params[:page], per_page: 60)  
+        @images = Image.where(id: @images).joins(:categories).where(categories:{ id: category_id }).paginate(page: params[:page], per_page: 60)  
         
       else 
-        @images = Image.where(id: @images).order("RANDOM()").paginate(page: params[:page], per_page: 60)  
+        @images = Image.where(id: @images).paginate(page: params[:page], per_page: 60)  
         
       end 
       
@@ -45,7 +45,7 @@ class ImagesController < ApplicationController
         category_id = category.id unless category.nil?
         @images = Image.joins(:categories).where(categories: { id: category_id }).distinct.paginate(page: params[:page], per_page: 60)
       else 
-        @images = Image.order("RANDOM()").paginate(page: params[:page], per_page: 60) 
+        @images = Image.paginate(page: params[:page], per_page: 60) 
       end
     end 
   end
